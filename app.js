3be23736525ad069d2ed70848e96831883845e3f
meckert <mathias.eckert@autoscout24.com>
2015-09-14 17:03:07 +0200

    added basic implementation + initial package.json file

---
+++ app.js
@@ -0,0 +1,30 @@
+//TODO: add to package.json
+var express = require('express');
+var bodyParser = require('body-parser');
+var bot = require('/.bot.js');
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+// body parser middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// test route
+app.get('/', function (req, res) { res.status(200).send('Hello world!') });
+
+// error handler
+app.use(function (err, req, res, next) {
+    console.error(err.stack);
+    res.status(400).send(err.message);
+});
+
+app.listen(port, function () {
+    console.log('Slack bot listening on port ' + port);
+});
+
+//listen to one of the following words:
+// yo mama, your mother, USER mama, USER mother --> show en joke, show personalized en joke
+// Deine Mudda, Deine Mutter, USER Mudda, USER Mutter --> show de joke, show personalized de joke
+
+// how do I listen to other commands?
+app.post('/hello', bot);
---
+++ bot.js
@@ -0,0 +1,61 @@
+var fs = require('fs');
+
+function getRandomJoke(jokes) {
+    var randomLine = Math.floor(Math.random() * jokes.length);
+    return jokes[randomLine];
+}
+
+function readLinesFromFile(language) {
+    //TODO: is there a readLines? async?
+    try {
+        var filename = __dirname + "/jokes/" + language + ".txt"
+        var jokes = fs.readFileSync(filename, 'UTF8');
+        return jokes.trim().split(/\n/);
+    } catch(e) {
+        console.log(e);
+    }
+}
+
+function personalizeJoke(joke, userName, lang) {
+    var prefix = lang == 'de' ? 'Deine' : 'Yo'; //TODO: make sure everything starts with Deine or Yo
+    return joke.replace(prefix, userName + '\'s');
+}
+
+function contains(str1, str2) {
+    return str1.indexOf(str2) > -1;
+}
+
+function getUserLanguageString(userInput) {
+    var lowerCaseInput = userInput.toLowerCase();
+
+    var lang = contains(lowerCaseInput, 'mutter') ||
+               contains(lowerCaseInput, 'mudda') ? 'de' :
+                contains(lowerCaseInput, 'mother') ||
+                contains(lowerCaseInput, 'moma') ? 'en' : 'en';
+
+    return lang;
+}
+
+//TODO: think about personalization
+function getJoke(userInput) {
+    var langString = getUserLanguageString(userInput)
+    var lines = readLinesFromFile(langString);
+    return getRandomJoke(lines);
+}
+
+console.log(getJoke('Yo mama'));
+
+
+//module.exports = function (req, res, next) {
+//    var userName = req.body.user_name;
+//    var botPayload = {
+//        text : joke
+//    };
+//
+//    // avoid infinite loop
+//    if (userName !== 'slackbot') {
+//        return res.status(200).json(botPayload);
+//    } else {
+//        return res.status(200).end();
+//    }
+//}
