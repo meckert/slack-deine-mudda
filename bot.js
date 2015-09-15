021551381d7c047819b7486d2a61c99b1d918a61
meckert <mathias.eckert@autoscout24.com>
2015-09-15 21:57:52 +0200

    returning json response for slack bot

--- bot.js
+++ bot.js
@@ -27,20 +27,18 @@ function getJoke() {
     return getRandomJoke(lines);
 }
 
-module.exports = {
-  getJoke: getJoke
-};
+module.exports = function (req, res, next) {
+    var userName = req.body.user_name;
+    var joke = getJoke();
+    var personalizedJoke = personalizeJoke(joke, userName, 'de');
+    var botPayload = {
+        text : personalizedJoke
+    };
 
-//module.exports = function (req, res, next) {
-//    var userName = req.body.user_name;
-//    var botPayload = {
-//        text : joke
-//    };
-//
-//    // avoid infinite loop
-//    if (userName !== 'slackbot') {
-//        return res.status(200).json(botPayload);
-//    } else {
-//        return res.status(200).end();
-//    }
-//}
+    // avoid infinite loop
+    if (userName !== 'slackbot') {
+        return res.status(200).json(botPayload);
+    } else {
+        return res.status(200).end();
+    }
+}
